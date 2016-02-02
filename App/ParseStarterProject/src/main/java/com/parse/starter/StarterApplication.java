1165fb2a1ae7576f638502c996573115d2d3d25e
P. R. Kumar <prkumar@rajs-macbook.dynamic.ucsd.edu>
2016-02-02 01:43:20 -0800

    Updated StaterApplication and strings.xml

--- App/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
+++ App/ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -13,6 +13,8 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseACL;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.parse.ParseException;
 
 
 public class StarterApplication extends Application {
@@ -32,5 +34,24 @@ public class StarterApplication extends Application {
     // Optionally enable public read access.
     // defaultACL.setPublicReadAccess(true);
     ParseACL.setDefaultACL(defaultACL, true);
+
+    ParseUser user = new ParseUser();
+    user.setUsername("my name");
+    user.setPassword("my pass");
+    user.setEmail("email@example.com");
+
+    // other fields can be set just like with ParseObject
+    user.put("phone", "650-555-0000");
+
+    user.signUpInBackground(new SignUpCallback() {
+      public void done(ParseException e) {
+        if (e == null) {
+          // Hooray! Let them use the app now.
+        } else {
+          // Sign up didn't succeed. Look at the ParseException
+          // to figure out what went wrong
+        }
+      }
+    });
   }
 }
