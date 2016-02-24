0071db82b068a90de40361715ab9c9fd679f0970
P. R. Kumar <prkumar@rajs-macbook.dynamic.ucsd.edu>
2016-02-24 13:55:35 -0800

    Check for username and email uniqueness.

--- App/app/src/main/java/com/cse110/app/Register.java
+++ App/app/src/main/java/com/cse110/app/Register.java
@@ -9,21 +9,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import java.util.List;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
 
     Button bRegister;
-    EditText etUsername, etPassword;
+    EditText etEmail, etPassword, etUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        etUsername = (EditText) findViewById(R.id.etEmail);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etEmail = (EditText) findViewById(R.id.etEmail);
         etPassword = (EditText) findViewById(R.id.etPassword);
 
         bRegister = (Button) findViewById(R.id.bRegister);
@@ -34,26 +40,45 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bRegister:
-                if (etUsername.getText().toString().endsWith("@ucsd.edu")) {
-                    ParseUser user = new ParseUser();
-                    user.setUsername(etUsername.getText().toString());
-                    user.setPassword(etPassword.getText().toString());
-                    user.setEmail(etUsername.getText().toString());
+
+        if (v.getId() == R.id.bRegister){
+            String emailAddress = etEmail.getText().toString();
+            if (emailAddress.endsWith("@ucsd.edu")) {
+
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                currentUser.logOut();
+
+                ParseUser user = new ParseUser();
+                user.setUsername(etUsername.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+                user.setEmail(etEmail.getText().toString());
 
 
-                    user.signUpInBackground(new SignUpCallback() {
-                        public void done(ParseException e) {
-                            if (e == null) {
-                                // Hooray! Let them use the app now.
-                            } else {
-                                // Sign up didn't succeed. Look at the ParseException
-                                // to figure out what went wrong
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            // Hooray! Let them use the app now.
+                        } else {
+
+                            switch (e.getCode()) {
+                                case ParseException.EMAIL_TAKEN: {
+                                    etEmail.setError("Email taken.");
+                                    break;
+                                }
+                                case ParseException.USERNAME_TAKEN: {
+
+                                    etUsername.setError("Username taken.");
+
+
+                                }
                             }
                         }
-                    });
-                }
+                    }
+                });
+            }
+            else {
+                etEmail.setError("Email must be an @ucsd.edu account!");
+            }
         }
     }
 
